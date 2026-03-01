91188ae4118fd1ef771d24e86a788bc78e530f65
dipterix <dipterix.wang@gmail.com>
2026-03-01 18:52:25 -0500

    Added drawer_ui example

--- inst/builtin-templates/bslib-bare/R/common.R
+++ inst/builtin-templates/bslib-bare/R/common.R
@@ -1,18 +1,20 @@
 library(shiny)
-page_title <- function(complete = TRUE){
-  if(complete){
+page_title <- function(complete = TRUE) {
+  if (complete) {
     "Shiny Dashboard Template - bslib"
   } else {
     "ShiDashi"
   }
 }
-page_logo <- function(size = c("normal", "small", "large")){
+
+page_logo <- function(size = c("normal", "small", "large")) {
   "shidashi/img/icon.png"
 }
-page_loader <- function(){
+page_loader <- function() {
   NULL
 }
-body_class <- function(){
+
+body_class <- function() {
   c(
     #--- Start as dark-mode ---
     "dark-mode",
@@ -21,7 +23,8 @@ body_class <- function(){
     "shidashi-drawer-no-overlay"
   )
 }
-nav_class <- function(){
+
+nav_class <- function() {
   c(
     "shidashi-header",
     "navbar",
@@ -29,16 +32,16 @@ nav_class <- function(){
   )
 }
 
-sidebar_class <- function(){
+sidebar_class <- function() {
   c(
     #--- Start as dark-mode ---
     "dark-mode"
   )
 }
 
-module_breadcrumb <- function(){}
+module_breadcrumb <- function() {}
 
-drawer_ui <- function(){
+drawer_ui <- function() {
   # Return the inner content for the drawer panel.
   # The outer .shidashi-drawer wrapper is provided by index.html.
   # Override this function in your project's R/common.R to add
@@ -47,17 +50,26 @@ drawer_ui <- function(){
     shiny::h5("Settings"),
     shiny::p(
       "This is the right-side drawer panel. ",
-      "Customize this in ", shiny::tags$code("R/common.R"),
-      " by editing the ", shiny::tags$code("drawer_ui()"), " function."
+      "Customize this in ",
+      shiny::tags$code("R/common.R"),
+      " by editing the ",
+      shiny::tags$code("drawer_ui()"),
+      " function."
     ),
     shiny::hr(),
     shiny::tags$small(
-      "Open with ", shiny::tags$code("drawer_open()"),
-      " or the ", shiny::tags$i(class = "fas fa-cog"), " icon."
+      "Open with ",
+      shiny::tags$code("drawer_open()"),
+      " or the ",
+      shiny::tags$i(class = "fas fa-cog"),
+      " icon."
     ),
     shiny::hr(),
-    shiny::p("Current module info can be obtained via ",
-             shiny::tags$code("shidashi::active_module()"), ":"),
+    shiny::p(
+      "Current module info can be obtained via ",
+      shiny::tags$code("shidashi::active_module()"),
+      ":"
+    ),
     shiny::verbatimTextOutput("drawer_output")
   )
 }
